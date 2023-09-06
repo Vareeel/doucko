05cf75fbf62406378e399c2e257836155231f465
Vareel <capousek@spstrutnov.cz>
2023-09-06 18:39:49 +0200

    Registrace + Prihlasování bez ukládání

--- src/app/controllers/uzivatelController.js
+++ src/app/controllers/uzivatelController.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 
 exports.prihlaseni = (req, res)=> { //stejny s roterem
     res.render('uzivatele/prihlaseni', {  //stejny s ejs souborem
@@ -9,6 +10,21 @@ exports.registrace = (req, res)=> { //stejny s roterem
     res.render('uzivatele/registrace', {  //stejny s ejs souborem
         titulek: 'Index moment',
     });
+
 }
 
 
+
+exports.registrovani = (req, res)=> {
+    const email = req.body.email
+    const heslo = req.body.heslo
+    const heslo2 = req.body.heslo2
+
+    return res.redirect("/uzivatele/prihlaseni")  //rediractování musí byt lomeno na zacatku, aleeeee v tom normalnim nahore neee!!!!!! picovina to jeee
+}
+
+exports.prihlasit = (req, res)=> {
+    const email = req.body.email
+    const heslo = req.body.heslo
+    return res.redirect("/web/index")
+}
--- src/app/routers/uzivateleRouter.js
+++ src/app/routers/uzivateleRouter.js
@@ -4,4 +4,11 @@ const router = express.Router();
 router.get('/prihlaseni', require('../controllers/uzivatelController').prihlaseni); //modry stejny jako v controlleru
 router.get('/registrace', require('../controllers/uzivatelController').registrace); //modry stejny jako v controlleru
 
+
+router.post('/registrace', require('../controllers/uzivatelController').registrovani); //musí být jiné než getovací   //modrý musí být vždy jiný//
+router.post('/prihlaseni', require('../controllers/uzivatelController').prihlasit); //musí být jiné než getovací   //modrý musí být vždy jiný//
+
+
+
+
 module.exports = router;
